d6923f92868e38a3144c0f64a4bef5843edf80a1
Oliver Max Løye <oliverloye@me.com>
2019-05-22 23:44:54 +0200

    first commit

---
+++ lynda-exercise/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
+const PORT = 22334;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
+});
+
+const root = { friend: () => {
+    return {
+        "id": 1234123,
+        "firstName": "Oliver",
+        "lastName": "Mac",
+        "gender": "male",
+        "language": "Danish",
+        "email": "ol@ol.dk"
+    }
+} };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
+app.listen(PORT, () => console.log('Running server on port localhost:' + PORT + '/graphql'));
